a9fd4d7a2685f3fcddc30238aebe3c9030f16048
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-18 17:33:27 -0300

    fdsdfs

--- src/main/java/one/digitalinnovation/personapi/controller/PetController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PetController.java
@@ -7,6 +7,7 @@ import one.digitalinnovation.personapi.exception.PetNotFoundException;
 import one.digitalinnovation.personapi.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://arcadenoecliente.herokuapp.com")
 @RequestMapping("/api/v1/pet")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PetController {
